cfc856bd60bbd6f205f2787f584efdf52b62d08a
jejecrunch <gkswlgp456@gmail.com>
2020-09-28 13:24:41 +0900

    Example5a 코드에 Buffer가 붙여진 코드

---
+++ src/chap01/Example5b.java
@@ -0,0 +1,32 @@
+package chap01;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Example5b {
+
+    public static String readFile(String filePath, String encoding) throws IOException {
+        InputStream in = new FileInputStream(filePath);
+        InputStreamReader reader1 = new InputStreamReader(in, encoding);
+        BufferedReader reader2 = new BufferedReader(reader1);
+        StringBuilder builder = new StringBuilder();
+        char[] a = new char[1024];
+        while (true) {
+            int count = reader2.read(a);
+            if (count == -1) break;
+            builder.append(a, 0, count);
+        }
+        reader2.close();
+        return builder.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String s = readFile("d:/1_university/000 전공/소프트웨어공학과/강의내용정리/3학년 2학기/네트워크프로그래밍/실습/temp/file3.txt", "UTF-8");
+        System.out.println(s);
+    }
+
+}
+
